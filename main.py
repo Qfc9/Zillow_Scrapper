fcbf9ae9f93e027350ec1f77d27f4a98722bb033
Elijah Harmon <elijahharmon@gmail.com>
2018-03-26 08:13:45 -0400

    Updated with new filters

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
 from scrapper import *
 
-kingCounty = "https://www.zillow.com/homes/fsbo/King-County-WA/207_rid/globalrelevanceex_sort/48.167917,-120.653229,46.687131,-122.954865_rect/8_zm/"
-pierceCounter = "https://www.zillow.com/homes/fsbo/Pierce-County-WA/1322_rid/globalrelevanceex_sort/47.62838,-121.235505,46.500282,-122.993317_rect/8_zm/"
-
+kingCounty = "https://www.zillow.com/homes/fsbo/King-County-WA/house,condo,mobile,townhouse_type/207_rid/1-_beds/150000-_price/592-_mp/globalrelevanceex_sort/47.989921,-120.925141,46.86958,-122.682953_rect/8_zm/"
+pierceCounter = "https://www.zillow.com/homes/fsbo/Pierce-County-WA/house,condo,mobile,townhouse_type/1322_rid/1-_beds/149961-_price/592-_mp/globalrelevanceex_sort/47.62838,-121.235505,46.500282,-122.993317_rect/8_zm/"
 
 def main():
     msg = []
